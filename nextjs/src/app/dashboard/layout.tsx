5dc8409c54f10f8274e6c7ff87ebf3d21d4fd492
Dmitry Lenchevsky <49116427+DimitryLenchevsky@users.noreply.github.com>
2026-08-31 16:07:31 +0200

    pre-components-layout

--- nextjs/src/app/dashboard/layout.tsx
+++ nextjs/src/app/dashboard/layout.tsx
@@ -1,59 +1,14 @@
-"use client";
-
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState, type ReactNode } from "react";
+import { Sidebar } from "@/src/components/layout/sidebar";
+import type { ReactNode } from "react";
 
 interface DashboardLayoutProps {
   children: ReactNode;
 }
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-  const [text, setText] = useState("");
-  const router = useRouter();
-
   return (
     <div className="flex flex-row min-h-[calc(100vh-2rem)] border-4 border-blue-500 rounded-xl m-4 overflow-hiddent">
-      <aside className="w-64 bg-blue-50 p-6 border-r border-blue-100 flex flex-col">
-        <div className="mb-8 text-xs font-bold text-blue-500 uppercase tracking-widest font-sans">
-          Dashboard Layout
-        </div>
-
-        <nav className="flex flex-col gap-4">
-          <span className="font-bold text-sm text-zinc-400">Навигация</span>
-          <Link
-            href="/dashboard"
-            className="text-zinc-950 hover:text-blue-600 transition-colors"
-          >
-            {" "}
-            Главная{" "}
-          </Link>
-          <Link
-            href="/dashboard/settings"
-            className="text-zinc-950 hover:text-blue-600 transition-colors"
-          >
-            {" "}
-            Настройки{" "}
-          </Link>
-          <button onClick={() => router.push('/')} className="hover:text-red-600 transition-colors">
-            {" "}
-            Выйти{" "}
-          </button>
-        </nav>
-
-        <div className="mt-auto pt-6 border-t border-blue-200">
-          <label className="block text-[10px] font-bold text-blue-400 uppercase mb-2">
-            Persistent State (Layout)
-          </label>
-          <input
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            placeholder="Текст сохраниться..."
-            className="w-full p-2 text-sm border border-blue-200 rounded bg-white text-black focus:ring-2 focus:ring-blue-500 outline-none"
-          />
-        </div>
-      </aside>
+      <Sidebar />
 
       <main className="flex-1 flex flex-col bg-white p-8">{children}</main>
     </div>
